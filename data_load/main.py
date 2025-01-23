4027f5ad8ec6b76f528856a8a2af1e6109fdf20d
monetenmangel <76557453+monetenmangel@users.noreply.github.com>
2025-01-23 09:34:42 +0100

    Refactor timing variables and improve print statements for clarity

--- data_load/main.py
+++ data_load/main.py
@@ -3,23 +3,23 @@ import pandas as pd
 import os
 import time
 
-startzeit = time.time() # Startzeit des Skripts, um zu messen wie lange es braucht
+start = time.time()
 
 ARTIST_ID = 820 # Mac Millers Artist ID on Genius
 
+# Get list of all tracks, that Genius knows for Mac Miller
 genius_api_token = GeniusAPI.get_access_token()
 artist_songs = GeniusAPI.get_artist_songs(ARTIST_ID, genius_api_token)
 print("List with song URLs is finished")
 
+# Scrape the lyrics for all found tracks
 df = ScrapeSongs.scrape_all_lyrics(artist_songs)
 
-
+# Save results in csv
 output_dir = os.path.join(os.getcwd(), "output")
-
-# CSV-Datei im "output"-Ordner speichern
 output_path = os.path.join(output_dir, "lyrics.csv")
 df.to_csv(output_path, index=False, encoding="utf-8")
 
-endzeit = time.time()
+end = time.time()
 
-print(f"Success! Laufzeit: {endzeit - startzeit:.6f} Sekunden")
+print(f"Success! Runtime: {end - start:.6f} seconds")
